5d764a5c8979f667490296529d758a52a258fc5a
Florante Catapang <florantecatapang6@gmail.com>
2025-07-23 03:42:00 +0000

    Uncaught (in promise) Error: An error occurred in the Server Components

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ const dbCredentials = {
   port: 3306,
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
+  database: process.env.DB_DATABASE!,
 };
 
 
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,8 +3,8 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 
-if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
-  throw new Error("Missing database credentials in .env file. Please provide DB_HOST, DB_USER, DB_PASSWORD, and DB_NAME.");
+if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_DATABASE) {
+  throw new Error("Missing database credentials in .env file. Please provide DB_HOST, DB_USER, DB_PASSWORD, and DB_DATABASE.");
 }
 
 const connectionConfig = {
@@ -12,7 +12,7 @@ const connectionConfig = {
   port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
 };
 
 const poolConnection = mysql.createPool(connectionConfig);
