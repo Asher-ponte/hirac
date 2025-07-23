67129216a4c7b5c204db657ac7b155d232ac6c24
Florante Catapang <florantecatapang6@gmail.com>
2025-07-23 03:16:42 +0000

    still cant able to publish this project why ?

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,16 +2,12 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
-  throw new Error("Missing database credentials in .env file. Please provide DB_HOST, DB_USER, DB_PASSWORD, and DB_NAME.");
-}
-
 const dbCredentials = {
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST!,
   port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
 };
 
 
