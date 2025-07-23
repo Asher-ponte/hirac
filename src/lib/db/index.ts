611889323b862fd2b1a82ea5640ca8af98e100d3
Florante Catapang <florantecatapang6@gmail.com>
2025-07-23 02:39:49 +0000

    DB_HOST=34.44.106.215 DB_PORT=3306 DB_USER=florante DB_PASSWORD=Handsome

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,21 +3,16 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 
-if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
-  throw new Error("Missing database credentials in .env file. Please provide DB_USER, DB_PASSWORD, and DB_NAME.");
-}
-
-if (!process.env.DB_HOST && !process.env.DB_SOCKET_PATH) {
-    throw new Error("Missing database connection details in .env file. Please provide either DB_HOST or DB_SOCKET_PATH.");
+if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+  throw new Error("Missing database credentials in .env file. Please provide DB_HOST, DB_USER, DB_PASSWORD, and DB_NAME.");
 }
 
 const connectionConfig = {
+  host: process.env.DB_HOST,
+  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ...(process.env.DB_SOCKET_PATH
-    ? { socketPath: process.env.DB_SOCKET_PATH }
-    : { host: process.env.DB_HOST, port: 3306 }),
 };
 
 const poolConnection = mysql.createPool(connectionConfig);
